ba2297f43e9f26098874c5f132b7fe55b82457a2
Gleb Bond <gebond_77@mail.ru>
2016-02-29 19:34:39 +0300

    first creating

---
+++ Diploma/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Diploma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heelooo! 12123123123123");
+        }
+    }
+}
